3a4fdeb522b85a38fd98518ff40c063f1320c7a7
Tanya-abi <tchigwda6@gmail.com>
2024-09-10 22:04:15 +0200

    CS50 Web dev

---
+++ CS50 Web development/Practice/Lecture 2/something.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+a = 28 #integer
+b = 1.5 #Float
+c = "Hello" # String
+d = True #Boolean
+e = None #Null
+
+#name = input("Name: ")
+#print("Hello ", + name)
+
+name = input("Name: ")
+print("Hello, " + name)
